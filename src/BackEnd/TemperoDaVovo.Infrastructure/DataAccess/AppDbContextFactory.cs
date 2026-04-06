f62b945671896ed92e42f6fa9ec181e0af021a1c
Wesley Neves <wellynvs@gmail.com>
2026-04-06 20:55:48 -0300

    new initial migration

---
+++ src/BackEnd/TemperoDaVovo.Infrastructure/DataAccess/AppDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace TemperoDaVovo.Infrastructure.DataAccess;
+
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+        optionsBuilder.UseNpgsql(
+            "Host=dpg-d7a42tnkijhs73dgtnlg-a.oregon-postgres.render.com;Port=5432;Database=temperodavovo_db_96b7;Username=temperodavovo_db_96b7_user;Password=8ri8hhClzgYbZn1JRXIhBeFPuGMmyylZ;SSL Mode=Require;Trust Server Certificate=true"
+        );
+
+        return new AppDbContext(optionsBuilder.Options);
+    }
+}
--- src/Shared/TemperoDaVovo.Exceptions/obj/Debug/net8.0/TemperoDaVovo.Exceptions.AssemblyInfo.cs
+++ src/Shared/TemperoDaVovo.Exceptions/obj/Debug/net8.0/TemperoDaVovo.Exceptions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TemperoDaVovo.Exceptions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7403c229f8945e9f5925a82fabaaae7a1fc4485a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3e7e9a0c51ddee84a66020a273fea8458374682")]
 [assembly: System.Reflection.AssemblyProductAttribute("TemperoDaVovo.Exceptions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TemperoDaVovo.Exceptions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
