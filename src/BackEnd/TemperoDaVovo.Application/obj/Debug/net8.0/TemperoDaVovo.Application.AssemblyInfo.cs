cb05d5aecb3a86ec1f76b8938b7c14b6f6c40a15
Wesley Neves <wellynvs@gmail.com>
2026-04-07 00:43:50 -0300

    NoConcurrency

--- src/BackEnd/TemperoDaVovo.Application/obj/Debug/net8.0/TemperoDaVovo.Application.AssemblyInfo.cs
+++ src/BackEnd/TemperoDaVovo.Application/obj/Debug/net8.0/TemperoDaVovo.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TemperoDaVovo.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43056d8f004c3a86d6749d429841ed9250dec5b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6b7aa894506156ee5a3d109a5bd161af88e7796")]
 [assembly: System.Reflection.AssemblyProductAttribute("TemperoDaVovo.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TemperoDaVovo.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
